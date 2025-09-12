b734cca3fbf3444eb274c0f61af3c14a8a82860f
hammad <hammadashfaq341@gmail.com>
2025-09-13 00:14:29 +0500

    Commit four

--- src/main/java/JavaPractice/DataType_Variable.java
+++ src/main/java/JavaPractice/DataType_Variable.java
@@ -7,14 +7,16 @@ public class DataType_Variable {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	   
-      int n=5;
-      int f=1;
-      for(int a=1;a<=n;a++)
+      int a=0,b=1,c;
+      int n=10;
+      System.out.print(a + " " +  b);
+      for(int i=2;i<=n;i++)
       {
-    	  f=f*a;
+    	  c=a+b;
+    	  a=b;
+    	  b=c;
+    	  System.out.print(" "+ c);
       }
-      System.out.println("Factorial of Number=" + f);
-     
         
      
 	}
