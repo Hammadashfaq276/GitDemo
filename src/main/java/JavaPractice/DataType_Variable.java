8a1b710f3c27b02e97710513206fcd6de8134266
hammad <hammadashfaq341@gmail.com>
2025-09-14 23:18:31 +0500

    seven commit

--- src/main/java/JavaPractice/DataType_Variable.java
+++ src/main/java/JavaPractice/DataType_Variable.java
@@ -7,25 +7,14 @@ public class DataType_Variable {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	   
-        int num=123;
-        int temp=num;
-        int rev=0;
-        while(temp>0)
-        {
-        	int last=temp % 10;
-        	rev=(rev*10)+last;
-        	temp=temp/10;
-        }
-        
-        if(num==rev)
-        {
-        	System.out.println(num + " Is Palindrome Number");
-        }
-        else
-        {
-        	System.out.println(num + " Is not Palindrome Number");
-        }
-        
+      int num=123,sum=0;
+      while(num>0)
+      {
+    	  int last=num%10;
+    	  sum=sum + last;
+    	  num=num/10;
+      }
+      System.out.println("Sum of digit=" + sum);
         
      
 	}
