0a4fb8320d26638ca765b84acb056c8b923972fb
hammad <hammadashfaq341@gmail.com>
2025-09-15 23:35:18 +0500

    eight commit

--- src/main/java/JavaPractice/DataType_Variable.java
+++ src/main/java/JavaPractice/DataType_Variable.java
@@ -7,14 +7,15 @@ public class DataType_Variable {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	   
-      int num=123,sum=0;
-      while(num>0)
-      {
-    	  int last=num%10;
-    	  sum=sum + last;
-    	  num=num/10;
-      }
-      System.out.println("Sum of digit=" + sum);
+       int rows=4;
+       for(int i=1;i<=rows;i++)
+       {
+    	   for(int j=1;j<=i;j++)
+    	   {
+    		   System.out.print("*");    	   
+    	   }
+    	   System.out.println();
+       }
         
      
 	}
