03449d25905d3db1e87f88fb847ff0a3d2d8256d
hammad <hammadashfaq341@gmail.com>
2025-09-23 23:32:53 +0500

    fourteen commit

--- src/main/java/JavaPractice/DataType_Variable.java
+++ src/main/java/JavaPractice/DataType_Variable.java
@@ -7,23 +7,13 @@ public class DataType_Variable {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	   
-      int n=4;
-      int max_width=2*n-1;
-      for(int i=0;i<n;i++)
+      int n=3;
+      for(int i=1;i<=n;i++)
       {
-    	  int stars=2*i+1;
-    	  int spaces=(max_width-stars)/2;
-    	  
-    	  for(int j=0;j<spaces;j++)
+    	  for(int j=1;j<=n;j++)
     	  {
-    		  System.out.print(" ");
+    		  System.out.println("(" + i + "," + j + ")");
     	  }
-    	  for(int j=0;j<stars;j++)
-    	  {
-    		  System.out.print("*");
-    	  }
-    	
-    	  System.out.println();
       }
         
      
