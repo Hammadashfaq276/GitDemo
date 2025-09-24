ebd070a8b03407413a053184353b40611c3de6ca
hammad <hammadashfaq341@gmail.com>
2025-09-24 22:58:32 +0500

    fourteen commit

--- src/main/java/JavaPractice/DataType_Variable.java
+++ src/main/java/JavaPractice/DataType_Variable.java
@@ -7,14 +7,16 @@ public class DataType_Variable {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	   
-      int n=3;
-      for(int i=1;i<=n;i++)
+      int n=10;
+      for(int i=1;i<=10;i++)
       {
     	  for(int j=1;j<=n;j++)
     	  {
-    		  System.out.println("(" + i + "," + j + ")");
+    		  System.out.print(i*j + " ");
     	  }
+    	  System.out.println();
       }
+      
         
      
 	}
